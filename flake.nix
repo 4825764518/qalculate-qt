29448e9d2a990b74974ae344dc15d2c1fa1e2b43
4825764518 <4825764518@purelymail.com>
2022-11-24 05:40:05 -0500

    Add nix shell and nix-direnv config

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ qt5.qmake intltool pkg-config ];
+          buildInputs = with pkgs; [ libqalculate qt5.qtbase qt5.qttools ];
+        };
+      });
+}
